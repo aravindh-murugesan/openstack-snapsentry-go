a5043b971fce9190d518675e1e320649e5ae6b45
Aravindh Murugesan <aravindhstanley@gmail.com>
2026-01-01 13:32:01 +0530

    feat(cli): add daemon mode for snapsentry
    A Basic cron style daemon mode is implemented. This allows flexibility
    to run this as a standalone pod/program as well as external schedulers
    like ArgoCD.

--- go.mod
+++ go.mod
@@ -4,7 +4,10 @@ go 1.25.5
 
 require (
 	github.com/charmbracelet/lipgloss v1.1.0
+	github.com/go-co-op/gocron-ui v0.2.0
+	github.com/go-co-op/gocron/v2 v2.19.0
 	github.com/go-viper/mapstructure/v2 v2.4.0
+	github.com/google/uuid v1.6.0
 	github.com/gophercloud/gophercloud/v2 v2.9.0
 	github.com/gophercloud/utils/v2 v2.0.0-20251121145439-0a38d66a3d88
 	github.com/lmittmann/tint v1.1.2
@@ -20,13 +23,18 @@ require (
 	github.com/charmbracelet/x/term v0.2.1 // indirect
 	github.com/fsnotify/fsnotify v1.9.0 // indirect
 	github.com/gofrs/uuid/v5 v5.4.0 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/gorilla/websocket v1.5.3 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/jonboulle/clockwork v0.5.0 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.16 // indirect
 	github.com/muesli/termenv v0.16.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.4 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/robfig/cron/v3 v3.0.1 // indirect
+	github.com/rs/cors v1.11.1 // indirect
 	github.com/sagikazarmark/locafero v0.11.0 // indirect
 	github.com/sourcegraph/conc v0.3.1-0.20240121214520-5f936abd7ae8 // indirect
 	github.com/spf13/afero v1.15.0 // indirect
---
+++ internal/cli/daemon.go
@@ -0,0 +1,137 @@
+package cli
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/aravindh-murugesan/openstack-snapsentry-go/internal/workflow"
+	"github.com/go-co-op/gocron-ui/server"
+	"github.com/go-co-op/gocron/v2"
+	"github.com/spf13/cobra"
+)
+
+var (
+	createSchedule string
+	expireSchedule string
+	bindAddress    string
+)
+
+var daemonCommand = &cobra.Command{
+	Use:     "daemon",
+	Short:   "Run Snapsentry in daemon mode",
+	GroupID: "snapsentry",
+	Long:    `Starts Snapsentry as a background service that continuously manages snapshot creation and expiry based on configured policies.`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		fmt.Println(headerStyle.Render("Snapsentry - Daemon Mode"))
+
+		dlog := workflow.SetupLogger(logLevel, cloudProfile).With("component", "daemon")
+
+		s, err := gocron.NewScheduler()
+		if err != nil {
+			return fmt.Errorf("failed to create scheduler: %w", err)
+		}
+		s.Start()
+		dlog.Info("Scheduler started", "cloud", cloudProfile)
+
+		// 1. Declare the variable first so it can be used INSIDE the task closure
+		var snapshotJob gocron.Job
+
+		// 2. Define the Job
+		snapshotJob, snapshotJobError := s.NewJob(
+			gocron.CronJob(
+				createSchedule,
+				false,
+			),
+			gocron.NewTask(func() {
+				// A. Run the Workflow
+				workflow.RunProjectSnapshotWorkflow(cloudProfile, timeout, logLevel)
+
+				// B. Calculate and Log the Next Run (Post-Execution)
+				if snapshotJob != nil {
+					if nextRun, err := snapshotJob.NextRun(); err == nil {
+						dlog.Info("Snapshot Workflow completed",
+							"next_run", nextRun.Format(time.RFC3339),
+							"job_id", snapshotJob.ID())
+					}
+				}
+			}),
+			gocron.WithName("Snapshot Creation Workflow"),
+			gocron.WithSingletonMode(gocron.LimitModeReschedule),
+		)
+		if snapshotJobError != nil {
+			return snapshotJobError
+		}
+
+		// 3. Log the Initial Next Run (Pre-Execution)
+		if nextRunSnapshot, err := snapshotJob.NextRun(); err == nil {
+			dlog.Info("Job Scheduled",
+				"job_name", snapshotJob.Name(),
+				"job_id", snapshotJob.ID(),
+				"schedule", createSchedule,
+				"next_run", nextRunSnapshot.Format(time.RFC3339))
+		}
+
+		// --- Expiry Workflow ---
+		var expireJob gocron.Job
+
+		expireJob, expireErr := s.NewJob(
+			gocron.CronJob(
+				expireSchedule,
+				false,
+			),
+			gocron.NewTask(func() {
+				// A. Run the Workflow
+				workflow.RunProjectSnapshotExpiryWorkflow(cloudProfile, timeout, logLevel, time.Now().UTC())
+
+				// B. Calculate and Log the Next Run (Post-Execution)
+				if expireJob != nil {
+					if nextRun, err := expireJob.NextRun(); err == nil {
+						dlog.Info("Snapshot Workflow completed",
+							"next_run", nextRun.Format(time.RFC3339),
+							"job_id", expireJob.ID())
+					}
+				}
+			}),
+			gocron.WithName("Snapshot Expiry Workflow"),
+			gocron.WithSingletonMode(gocron.LimitModeReschedule),
+		)
+		if expireErr != nil {
+			return expireErr
+		}
+
+		// 3. Log the Initial Next Run (Pre-Execution)
+		if nextRunSnapshot, err := expireJob.NextRun(); err == nil {
+			dlog.Info("Job Scheduled",
+				"job_name", expireJob.Name(),
+				"job_id", expireJob.ID(),
+				"schedule", expireSchedule,
+				"next_run", nextRunSnapshot.Format(time.RFC3339))
+		}
+
+		srv := server.NewServer(s, 8080, server.WithTitle("Snapsentry Go - Dashboard")) // with custom title if you want to customize the title of the UI (optional)
+		dlog.Info("Snapsentry Scheduler UI started", "address", bindAddress)
+		if err := http.ListenAndServe(bindAddress, srv.Router); err != nil {
+			dlog.Error("Failed to start UI server", "error", err)
+			return s.Shutdown()
+		}
+
+		// 4. Block Main Thread until Signal
+		sigChan := make(chan os.Signal, 1)
+		signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
+		<-sigChan
+
+		dlog.Warn("Shutting down scheduler due to system signal...")
+		return s.Shutdown()
+	},
+}
+
+func init() {
+	rootCommand.AddCommand(daemonCommand)
+	daemonCommand.Flags().StringVar(&createSchedule, "create-schedule", "*/10 * * * *", "Cron schedule for snapshot creation")
+	daemonCommand.Flags().StringVar(&expireSchedule, "expire-schedule", "0 */6 * * *", "Cron schedule for snapshot expiration")
+	daemonCommand.Flags().StringVar(&bindAddress, "bind-address", "0.0.0.0:8080", "Address to bind the UI server")
+}
